92ac5635b9de9bae4af7d06d1f3edea41b809d0a
yunsung <12345ggh81@gmail.com>
2021-07-12 16:00:44 +0900

    [CRATE] ViewModel Study

--- app/src/main/java/com/example/geoquiz/geoquizapp/QuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/geoquizapp/QuizViewModel.kt
@@ -1,7 +1,7 @@
-package com.example.geoquiz
+package com.example.geoquiz.geoquizapp
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.geoquiz.R
 
 private const val TAG = "QuizViewModel"
 
@@ -12,18 +12,19 @@ class QuizViewModel : ViewModel() {
 //    }
 
     //문제 리스트
-    private val questionBank = listOf(
+     val questionBank = listOf(
         Question(R.string.question_australia,true,userAnswer = false),
         Question(R.string.question_oceans,true,userAnswer = false),
         Question(R.string.question_mideast,false,userAnswer = false),
         Question(R.string.question_africa,false,userAnswer = false),
         Question(R.string.question_americas,true,userAnswer = false),
-        Question(R.string.question_asia,true, userAnswer = false))
+        Question(R.string.question_asia,true, userAnswer = false)
+    )
 
 
     //엑티비티 로직 코드를 액티비티와 분리 할 수 있어서 액티비티를 좀더 간단하게 유지가능!
     //액티비티에 추가되는 모든 코드는 뜻하지 않게 액티비티 생명주기를 따를 수 있기때문에 따로 분리해주는것이 좋다!
-    private var currentIndex = 0
+    var currentIndex = 0
 
     val currentQuestionAnswer : Boolean
     get() = questionBank[currentIndex].answer
